6f9172c6f7f9e064874ba4d5a1439897ae538a32
Ahmed-Atef-Adel <Ahmed-Atef-Adel.ahmedatef8885@gmail.com>
2025-04-17 05:48:00 +0200

    session3

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/home.html", {root: __dirname});
+  res.sendFile("./views/home.html", { root: __dirname });
 });
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`);
-});
+mongoose
+  .connect(
+    "mongodb+srv://ahmedatef8885:Password12345@cluster0.n8myt.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
