109715d1ca5eea61839e886f7f2a580f17427f4b
Ahmed-Atef-Adel <Ahmed-Atef-Adel.ahmedatef8885@gmail.com>
2025-07-05 14:20:10 +0300

    HashingPassword

--- models/authUser.js
+++ models/authUser.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 // define the schema structure
 const authUserSchema = new Schema({
   username: String,
   email: String,
   password: String,
+});
+
+// Hashing Password 
+
+authUserSchema.pre("save", async function (next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 // Create a model based on that schema
